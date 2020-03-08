d42f3ee5bd80c403388091c4eec7200ac6d6ef16
Tran Manh Khoa <fit.nuce@gmail.com>
2020-03-08 15:55:33 +0900

    added Start Game screen

--- App.js
+++ App.js
@@ -1,62 +1,20 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import MessageInput from './components/MessageInput';
-import ListText from './components/ListText';
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  const [listMessages, setListMessages] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
-
-  const addMessageHandler = messageTitle => {
-    if(messageTitle.length == 0){
-      return;
-    }
-    setListMessages(currentMessages => [
-      ...currentMessages,
-      { id: Math.random().toString(), value: messageTitle }
-    ]);
-    setIsAddMode(false);
-  };
-
-  const removeMessageHandler = messageID => {
-    setListMessages(currentMessages => {
-      return currentMessages.filter((message) => message.id !== messageID);
-    });
-  }
-
-  const resetMessageAdditionHandler = () => {
-    setIsAddMode(false);
-  };
-
   return (
-    <View style={styles.container}>
-      <Button title="Add new message" onPress={() => setIsAddMode(true)} />
-      <MessageInput visible={isAddMode} onAddMessage={addMessageHandler} onReset={resetMessageAdditionHandler} />
-      <FlatList
-        style={styles.viewContent}
-        keyExtractor={(item, index) => item.id}
-        data={listMessages}
-        renderItem={itemData => (
-          <ListText 
-            id={itemData.item.id} 
-            onDelete={removeMessageHandler} 
-            title={itemData.item.value} 
-          />
-        )}
-      />
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 50,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  viewContent: {
-    width: '80%',
-  },
+  screen: {
+    flex: 1
+  }
 });
